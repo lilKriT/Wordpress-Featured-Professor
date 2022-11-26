7501246ceebeda904f63e2ea82265f1ec1135b86
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-26 21:43:50 +0100

    Saving users choice.

--- src/index.js
+++ src/index.js
@@ -5,20 +5,35 @@ wp.blocks.registerBlockType("fprof/featured-professor", {
   description: "Include a featured block of your professor",
   icon: "welcome-learn-more",
   category: "common",
+  attributes: {
+    profID: { type: "string" },
+  },
   edit: EditComponent,
   save: function () {
     return null;
   },
 });
 
-function EditComponent() {
+function EditComponent(props) {
   return (
     <div className="featured-professor-wrapper">
       <div className="professor-select-container">
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
+        <select
+          onChange={(e) => {
+            props.setAttributes({ profID: e.target.value });
+            console.log(e.target.value);
+          }}
+        >
+          <option value="">Select a professor</option>
+          <option value="1" selected={props.attributes.profID == 1}>
+            1
+          </option>
+          <option value="2" selected={props.attributes.profID == 2}>
+            2
+          </option>
+          <option value="3" selected={props.attributes.profID == 3}>
+            3
+          </option>
         </select>
       </div>
       <div>The HTML preview of the selected professor will appear here.</div>
