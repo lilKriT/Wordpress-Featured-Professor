2f6334ebc6b474eefdd671f8c21af3489201d29e
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-27 22:40:05 +0100

    Displaying links and subjects.

--- inc/generateProfessorHTML.php
+++ inc/generateProfessorHTML.php
@@ -16,6 +16,25 @@ function generateProfessorHTML($id)
             <div class="professor-callout__text">
                 <h5><?php the_title(); ?></h5>
                 <p><?php echo wp_trim_words(get_the_content(), 30); ?></p>
+
+                <?php
+                $relatedPrograms = get_field("related_programs");
+                if ($relatedPrograms) {
+                ?>
+                    <p>Guy teaches:
+                        <?php
+                        foreach ($relatedPrograms as $key => $program) {
+                            echo get_the_title($program);
+                            if ($key != array_key_last($relatedPrograms) && count($relatedPrograms) > 1) {
+                                echo ", ";
+                            }
+                        }
+                        ?>.
+                    </p>
+                <?php
+                }
+                ?>
+                <p><strong><a href="<?php the_permalink(); ?>">Learn more about <?php the_title(); ?>&raquo;</a></strong></p>
             </div>
         </div>
 <?php
