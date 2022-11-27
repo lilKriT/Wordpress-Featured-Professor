878ba93e3ccbdacfeb70321439972e8fb374f628
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-27 22:33:00 +0100

    Displaying proper info.

--- inc/generateProfessorHTML.php
+++ inc/generateProfessorHTML.php
@@ -2,5 +2,24 @@
 
 function generateProfessorHTML($id)
 {
-    return "<div class='professor-callout'>Hello Professor!</div>";
+    $professorPost = new WP_Query(array(
+        "post_type" => "professor",
+        "p" => $id
+    ));
+
+    while ($professorPost->have_posts()) {
+        $professorPost->the_post();
+        ob_start(); ?>
+
+        <div class='professor-callout'>
+            <div class="professor-callout__photo" style="background-image: url(<?php the_post_thumbnail_url("professorPortrait"); ?>)"></div>
+            <div class="professor-callout__text">
+                <h5><?php the_title(); ?></h5>
+                <p><?php echo wp_trim_words(get_the_content(), 30); ?></p>
+            </div>
+        </div>
+<?php
+        wp_reset_postdata();
+        return ob_get_clean();
+    }
 }
